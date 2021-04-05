b468da3d87a8c164536fd0f2c2fddd152b4fd43c
xt00002003g <zailr20529@126.com>
2021-04-05 09:21:31 +0800

    Poskus z jupyter-server-proxy

--- primer.py
+++ primer.py
@@ -54,5 +54,5 @@ conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, passwo
 conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
 
-# poženemo strežnik na portu 8888, glej http://localhost:8888/
-run(host='localhost', port=8888)
+# poženemo strežnik na portu 8080, glej http://localhost:8080/
+run(host='localhost', port=8080)
