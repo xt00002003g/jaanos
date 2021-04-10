20f86e7404f2b1e26e3851cfd17d469f3e566954
xt00002003g <zailr20529@126.com>
2021-04-10 13:21:06 +0800

    Zagon brez povezave na bazo

--- primer.py
+++ primer.py
@@ -50,9 +50,9 @@ def dodaj_transakcijo_post():
 # Glavni program
 
 # priklopimo se na bazo
-conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
-conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
-cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
+#conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
+#conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
+#cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 # poženemo strežnik na portu 8080, glej http://localhost:8080/
 run(host='localhost', port=8080)
